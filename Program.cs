575483f0d7206331f8bcf1e65851ab81b0ceb58c
CJJ <ysharp.design@gmail.com>
2013-12-11 15:29:01 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -34,37 +34,6 @@ namespace Test
          * Warning: size is 189,9 MB."
          */
 
-        static void LoopTest(string parserName, Func<string, object> parseFunc, string testFile, int count)
-        {
-            Console.Clear();
-            Console.WriteLine("Parser: {0}", parserName);
-            Console.WriteLine();
-            Console.WriteLine("Loop Test File: {0}", testFile);
-            Console.WriteLine("Iterations: {0}", count.ToString("0,0"));
-            Console.WriteLine();
-            Console.WriteLine("Press ESC to skip this test or any other key to start...");
-            Console.WriteLine();
-            if (Console.ReadKey().KeyChar == 27)
-                return;
-
-            var json = System.IO.File.ReadAllText(testFile);
-            var st = DateTime.Now;
-            var l = new List<object>();
-            for (var i = 0; i < count; i++)
-                l.Add(parseFunc(json));
-            var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
-
-            System.Diagnostics.Debug.Assert(l.Count == count);
-
-            Console.WriteLine();
-            Console.WriteLine("... Done, in {0} ms. Throughput: {1} characters / seconds.", tm.ToString("0,0"), (1000 * (decimal)(count * json.Length) / (tm > 0 ? tm : 1)).ToString("0,0.00"));
-            Console.WriteLine();
-            GC.Collect();
-            Console.WriteLine("Press a key...");
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-
         /*  System.Text.Json.JsonParser's figures vs. JSON.NET's, v5.0 r8:
 
             (.NET 4.0 target, on Ideapad w/ Intel Core i5 CPU @ 2.50GHz, 6GB RAM, running Win7 64bit)
@@ -87,120 +56,189 @@ namespace Test
             "Huge" Test (180 mb JSON file):
             Parsed in ~ 9.75 seconds vs. JSON.NET 5.0 r8... OutOfMemoryException
          */
-        static void Test(string parserName, Func<string, object> parseFunc, string testFile)
-        {
-            Console.Clear();
-            Console.WriteLine("Parser: {0}", parserName);
-            Console.WriteLine();
-            Console.WriteLine("Test File: {0}", testFile);
-            Console.WriteLine();
-            Console.WriteLine("Press ESC to skip this test or any other key to start...");
-            Console.WriteLine();
-            if (Console.ReadKey().KeyChar == 27)
-                return;
 
-            var json = System.IO.File.ReadAllText(testFile);
-            var st = DateTime.Now;
-            var o = parseFunc(json);
-            var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
+		static void LoopTest(string parserName, Func<string, object> parseFunc, string testFile, int count)
+		{
+			Console.Clear();
+			Console.WriteLine("Parser: {0}", parserName);
+			Console.WriteLine();
+			Console.WriteLine("Loop Test File: {0}", testFile);
+			Console.WriteLine("Iterations: {0}", count.ToString("0,0"));
+			Console.WriteLine();
+			Console.WriteLine("Press ESC to skip this test or any other key to start...");
+			Console.WriteLine();
+			if (Console.ReadKey().KeyChar == 27)
+				return;
 
-            Console.WriteLine();
-            Console.WriteLine("... Done, in {0} ms. Throughput: {1} characters / seconds.", tm.ToString("0,0"), (1000 * (decimal)json.Length / (tm > 0 ? tm : 1)).ToString("0,0.00"));
-            Console.WriteLine();
-            if (o is FathersData)
-            {
-                Console.WriteLine("Fathers : {0}", ((FathersData)o).fathers.Length.ToString("0,0"));
-                Console.WriteLine();
-            }
-            GC.Collect();
-            Console.WriteLine("Press a key...");
-            Console.WriteLine();
-            Console.ReadKey();
-        }
+			System.Threading.Thread.MemoryBarrier();
+			var initialMemory = System.GC.GetTotalMemory(true);
 
-        public class Person
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public bool Married { get; set; }
-            public string Address { get; set; }
-            // Just to be sure we support that one, too:
-            public IEnumerable<int> Scores { get; set; }
-            public object Data { get; set; }
-        }
+			var json = System.IO.File.ReadAllText(testFile);
+			var st = DateTime.Now;
+			var l = new List<object>();
+			for (var i = 0; i < count; i++)
+				l.Add(parseFunc(json));
+			var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
 
-        public class FathersData
-        {
-            public Father[] fathers { get; set; }
-        }
+			System.Threading.Thread.MemoryBarrier();
+			var finalMemory = System.GC.GetTotalMemory(true);
+			var consumption = finalMemory - initialMemory;
 
-        public class Father
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-            public bool married { get; set; }
-            // Lists...
-            public List<Son> sons { get; set; }
-            // ... or arrays for collections, that's fine:
-            public Daughter[] daughters { get; set; }
-        }
+			System.Diagnostics.Debug.Assert(l.Count == count);
 
-        public class Son
-        {
-            public int age { get; set; }
-            public string name { get; set; }
-        }
+			Console.WriteLine();
+			Console.WriteLine("... Done, in {0} ms. Throughput: {1} characters / seconds.", tm.ToString("0,0"), (1000 * (decimal)(count * json.Length) / (tm > 0 ? tm : 1)).ToString("0,0.00"));
+			Console.WriteLine();
+			Console.WriteLine("\tMemory used : {0}", ((decimal)finalMemory).ToString("0,0"));
+			Console.WriteLine();
 
-        public class Daughter
-        {
-            public int age { get; set; }
-            public string name { get; set; }
-        }
+			GC.Collect();
+			Console.WriteLine("Press a key...");
+			Console.WriteLine();
+			Console.ReadKey();
+		}
 
-        static void SpeedTests()
-        {
-            LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 10000);
-            LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 10000);
-            LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 10000);
+		static void Test(string parserName, Func<string, object> parseFunc, string testFile)
+		{
+			Console.Clear();
+			Console.WriteLine("Parser: {0}", parserName);
+			Console.WriteLine();
+			Console.WriteLine("Test File: {0}", testFile);
+			Console.WriteLine();
+			Console.WriteLine("Press ESC to skip this test or any other key to start...");
+			Console.WriteLine();
+			if (Console.ReadKey().KeyChar == 27)
+				return;
 
-            LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 100000);
-            LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 100000);
-            LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 100000);
+			System.Threading.Thread.MemoryBarrier();
+			var initialMemory = System.GC.GetTotalMemory(true);
 
-            LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 1000000);
-            LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 1000000);
-            LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 1000000);
+			var json = System.IO.File.ReadAllText(testFile);
+			var st = DateTime.Now;
+			var o = parseFunc(json);
+			var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
 
-            LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().DeserializeObject, SMALL_TEST_FILE_PATH, 10000);
-            LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, SMALL_TEST_FILE_PATH, 10000);
-            LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<object>, SMALL_TEST_FILE_PATH, 10000);
+			System.Threading.Thread.MemoryBarrier();
+			var finalMemory = System.GC.GetTotalMemory(true);
+			var consumption = finalMemory - initialMemory;
 
-            LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().DeserializeObject, SMALL_TEST_FILE_PATH, 100000);
-            LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, SMALL_TEST_FILE_PATH, 100000);//(JSON.NET: OutOfMemoryException)
-            LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<object>, SMALL_TEST_FILE_PATH, 100000);
+			Console.WriteLine();
+			Console.WriteLine("... Done, in {0} ms. Throughput: {1} characters / seconds.", tm.ToString("0,0"), (1000 * (decimal)json.Length / (tm > 0 ? tm : 1)).ToString("0,0.00"));
+			Console.WriteLine();
+			Console.WriteLine("\tMemory used : {0}", ((decimal)finalMemory).ToString("0,0"));
+			Console.WriteLine();
 
-            var msJss = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
-            Test(typeof(JavaScriptSerializer).FullName, msJss.Deserialize<FathersData>, FATHERS_TEST_FILE_PATH);
-            Test("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<FathersData>, FATHERS_TEST_FILE_PATH);
-            Test(typeof(JsonParser).FullName, new JsonParser().Parse<FathersData>, FATHERS_TEST_FILE_PATH);
+			if (o is FathersData)
+			{
+				Console.WriteLine("Fathers : {0}", ((FathersData)o).fathers.Length.ToString("0,0"));
+				Console.WriteLine();
+			}
+			GC.Collect();
+			Console.WriteLine("Press a key...");
+			Console.WriteLine();
+			Console.ReadKey();
+		}
 
-            Test(typeof(JavaScriptSerializer).FullName, msJss.DeserializeObject, HUGE_TEST_FILE_PATH);
-            Test("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, HUGE_TEST_FILE_PATH);//(JSON.NET: OutOfMemoryException)
-            Test(typeof(JsonParser).FullName, new JsonParser().Parse<object>, HUGE_TEST_FILE_PATH);
+		public class Person
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public bool Married { get; set; }
+			public string Address { get; set; }
+			// Just to be sure we support that one, too:
+			public IEnumerable<int> Scores { get; set; }
+			public object Data { get; set; }
+		}
 
-            Console.Clear();
-            var stream = new System.IO.FileStream(FATHERS_TEST_FILE_PATH, System.IO.FileMode.Open);
-            Console.WriteLine("\"Fathers\" Test... streamed (press a key)");
-            Console.WriteLine();
-            Console.ReadKey();
-            var st = DateTime.Now;
-            var o = new JsonParser().Parse<FathersData>(stream);
-            System.Diagnostics.Debug.Assert(o.fathers.Length == 30000);
-            var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
-            Console.WriteLine();
-            Console.WriteLine("... {0} ms", tm);
-            Console.ReadKey();
-        }
+		public class FathersData
+		{
+			public Father[] fathers { get; set; }
+		}
+
+		public class Father
+		{
+			public int id { get; set; }
+			public string name { get; set; }
+			public bool married { get; set; }
+			// Lists...
+			public List<Son> sons { get; set; }
+			// ... or arrays for collections, that's fine:
+			public Daughter[] daughters { get; set; }
+		}
+
+		public class Son
+		{
+			public int age { get; set; }
+			public string name { get; set; }
+		}
+
+		public class Daughter
+		{
+			public int age { get; set; }
+			public string name { get; set; }
+		}
+
+		static void SpeedTests()
+		{
+			LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 10000);
+			LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 10000);
+			LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 10000);
+
+			LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 100000);
+			LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 100000);
+			LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 100000);
+
+			LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().Deserialize<Person>, TINY_TEST_FILE_PATH, 1000000);
+			LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<Person>, TINY_TEST_FILE_PATH, 1000000);
+			LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<Person>, TINY_TEST_FILE_PATH, 1000000);
+
+			LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().DeserializeObject, SMALL_TEST_FILE_PATH, 10000);
+			LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, SMALL_TEST_FILE_PATH, 10000);
+			LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<object>, SMALL_TEST_FILE_PATH, 10000);
+
+			LoopTest(typeof(JavaScriptSerializer).FullName, new JavaScriptSerializer().DeserializeObject, SMALL_TEST_FILE_PATH, 100000);
+			LoopTest("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, SMALL_TEST_FILE_PATH, 100000);//(JSON.NET: OutOfMemoryException)
+			LoopTest(typeof(JsonParser).FullName, new JsonParser().Parse<object>, SMALL_TEST_FILE_PATH, 100000);
+
+			var msJss = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
+			Test(typeof(JavaScriptSerializer).FullName, msJss.Deserialize<FathersData>, FATHERS_TEST_FILE_PATH);
+			Test("JSON.NET 5.0 r8", JsonConvert.DeserializeObject<FathersData>, FATHERS_TEST_FILE_PATH);
+			Test(typeof(JsonParser).FullName, new JsonParser().Parse<FathersData>, FATHERS_TEST_FILE_PATH);
+
+			Test(typeof(JavaScriptSerializer).FullName, msJss.DeserializeObject, HUGE_TEST_FILE_PATH);
+			Test("JSON.NET 5.0 r8", JsonConvert.DeserializeObject, HUGE_TEST_FILE_PATH);//(JSON.NET: OutOfMemoryException)
+			Test(typeof(JsonParser).FullName, new JsonParser().Parse<object>, HUGE_TEST_FILE_PATH);
+
+			StreamTest();
+		}
+
+		static void StreamTest()
+		{
+			Console.Clear();
+
+			System.Threading.Thread.MemoryBarrier();
+			var initialMemory = System.GC.GetTotalMemory(true);
+
+			var stream = new System.IO.FileStream(FATHERS_TEST_FILE_PATH, System.IO.FileMode.Open);
+			Console.WriteLine("\"Fathers\" Test... streamed (press a key)");
+			Console.WriteLine();
+			Console.ReadKey();
+			var st = DateTime.Now;
+			var o = new JsonParser().Parse<FathersData>(stream);
+			var tm = (int)DateTime.Now.Subtract(st).TotalMilliseconds;
+
+			System.Threading.Thread.MemoryBarrier();
+			var finalMemory = System.GC.GetTotalMemory(true);
+			var consumption = finalMemory - initialMemory;
+
+			System.Diagnostics.Debug.Assert(o.fathers.Length == 30000);
+			Console.WriteLine();
+			Console.WriteLine("... {0} ms", tm);
+			Console.WriteLine();
+			Console.WriteLine("\tMemory used : {0}", ((decimal)finalMemory).ToString("0,0"));
+			Console.WriteLine();
+			Console.ReadKey();
+		}
 
         static void Main(string[] args)
         {
